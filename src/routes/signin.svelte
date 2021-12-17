1531562f843859594fa7e7c6acc96e0cc9c8e4e2
flouvet <florian.louvet@railnova.eu>
2021-12-17 13:00:24 +0100

    fix icon in manifest and style login page

--- src/routes/signin.svelte
+++ src/routes/signin.svelte
@@ -18,29 +18,84 @@
 </script>
 
 <div>
-	<form class="new" action="/signin.json" method="post">
-		{#if error}
-			<p>{error}</p>
-		{/if}
-		<input
-			name="email"
-			id="email"
-			aria-label="Email"
-			placeholder="your@email.com"
-		/>
-		<input
-			type="password"
-			name="password"
-			aria-label="Password"
-			placeholder="Password"
-			id="password"
-		/>
-		<button type="submit">
-			{#if loading}
-				Loading...
-			{:else}
-				Submit
-			{/if}
-		</button>
+	<script context="module">
+		/** @type {import('@sveltejs/kit').Load} */
+		export async function load({ fetch, page }) {
+			const error = page.query.get('error');
+			const res = await fetch('/users.json');
+
+			if (res.ok) {
+				return {
+					props: {
+						users: await res.json(),
+						error: error,
+					},
+				};
+			}
+
+			const { message: error_message } = await res.json();
+			return {
+				status: res.status,
+				error: new Error(error_message),
+			};
+		}
+	</script>
+
+	<script>
+		export let users = null;
+		export let error = null;
+	</script>
+
+	<h1 class="text-primary text-2xl m-4">Login</h1>
+	{#if error}
+		<div class="alert alert-error m-4">
+			<div class="flex-1">
+				<svg
+					xmlns="http://www.w3.org/2000/svg"
+					fill="none"
+					viewBox="0 0 24 24"
+					class="w-6 h-6 mx-2 stroke-current"
+				>
+					<path
+						stroke-linecap="round"
+						stroke-linejoin="round"
+						stroke-width="2"
+						d="M18.364 18.364A9 9 0 005.636 5.636m12.728 12.728A9 9 0 015.636 5.636m12.728 12.728L5.636 5.636"
+					/>
+				</svg>
+				<label>{error}</label>
+			</div>
+		</div>
+	{/if}
+	<form action="/signin.json" method="post" class="flex flex-col items-stretch">
+		<div class="px-6 card bordered">
+			<div class="form-control my-3">
+				<label class="label" for="email">
+					<span class="label-text">Email</span>
+				</label>
+				<input
+					type="email"
+					class="input input-bordered"
+					name="email"
+					id="email"
+					aria-label="Email"
+					placeholder="your@email.com"
+				/>
+			</div>
+			<div class="form-control my-3">
+				<label class="label" for="password">
+					<span class="label-text">Password</span>
+				</label>
+				<input
+					class="input input-bordered"
+					type="password"
+					name="password"
+					aria-label="Password"
+					placeholder="Password"
+					id="password"
+				/>
+			</div>
+			<button class="btn btn-primary" type="submit">Login</button>
+		</div>
 	</form>
 </div>
